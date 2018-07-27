c77f12c51f2aab597763cd18b1dba25adab4f6e2
Neeraj Kilhore <ZKILNEE@ericsson.com>
2018-07-27 10:39:05 +0530

    api test1

--- conf.js
+++ conf.js
@@ -1,22 +1,28 @@
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 var HtmlReporter = require('protractor-beautiful-reporter');
+var http = require('http');
+
 
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['testCases/*js'],
+//  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['APITestCases/*js'],
   framework: 'jasmine',
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
   },
 
-  multiCapabilities: [
+ /* multiCapabilities: [
     {
       browserName: 'firefox',
     },
     {
       browserName: 'chrome',
     }
-  ],
+  ],*/
+jasmineNodeOpts:{
+  showColors: true,
+  defaultTimeoutInterval: 30000
+},
 
   onPrepare: function () {
     // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
