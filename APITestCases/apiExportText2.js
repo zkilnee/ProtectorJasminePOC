dca5a29633d10e790be973cd46b1bc49e8d8e354
Neeraj Kilhore <neeraj.kilhore@globallogic.com>
2018-08-10 16:32:20 +0530

    adding export cont

---
+++ APITestCases/apiExportText2.js
@@ -0,0 +1,13 @@
+var apiTest1 = require('./APIExportTest');
+var numAPIsExport= require('./APITestRest');
+/*
+var test = function(){
+
+   console.log(apiTest());
+}
+
+test();
+*/
+console.log(apiTest1());
+console.log(numAPIsExport());
+
